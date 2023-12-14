3229b984d1be0a87aa3e0e03e95aa2bda34bd965
olivas-afonso <127326931+olivas-afonso@users.noreply.github.com>
2023-12-14 14:55:44 +0000

    i2c working project ( with printing temp on lcd)

---
+++ i2c_working/Debug/i2c.d
@@ -0,0 +1,66 @@
+# FIXED
+
+i2c.obj: ../i2c.c
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/stdarg.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/stdbool.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/yvals.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/stdarg.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/linkage.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/_lock.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/stdint.h
+i2c.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_i2c.h
+i2c.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h
+i2c.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h
+i2c.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_gpio.h
+i2c.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/i2c.h
+i2c.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h
+i2c.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h
+i2c.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/math.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/ymath.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/float.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/_math_config.h
+i2c.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/errno.h
+
+../i2c.c:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/stdarg.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/stdbool.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/yvals.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/stdarg.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/linkage.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/_lock.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/stdint.h:
+
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_i2c.h:
+
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h:
+
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h:
+
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_gpio.h:
+
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/i2c.h:
+
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h:
+
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h:
+
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/math.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/ymath.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/float.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/_math_config.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_5.2.9/include/errno.h:
+
---
+++ i2c_working/Debug/subdir_vars.mk
@@ -0,0 +1,47 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../blinky_ccs.cmd 
+
+C_SRCS += \
+../LCD.c \
+../blinky.c \
+../i2c.c \
+../startup_ccs.c 
+
+C_DEPS += \
+./LCD.d \
+./blinky.d \
+./i2c.d \
+./startup_ccs.d 
+
+OBJS += \
+./LCD.obj \
+./blinky.obj \
+./i2c.obj \
+./startup_ccs.obj 
+
+OBJS__QUOTED += \
+"LCD.obj" \
+"blinky.obj" \
+"i2c.obj" \
+"startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"LCD.d" \
+"blinky.d" \
+"i2c.d" \
+"startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../LCD.c" \
+"../blinky.c" \
+"../i2c.c" \
+"../startup_ccs.c" 
+
+
