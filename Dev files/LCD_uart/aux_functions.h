4d9ccb6a384fc19d3285105d5f394ab6dd1630f9
olivas-afonso <olivas.afonso@gmail.com>
2024-01-08 09:50:07 +0000

    final commit

---
+++ Dev files/LCD_uart/aux_functions.h
@@ -0,0 +1,17 @@
+/*
+ * aux_functions.h
+ *
+ *  Created on: 16/12/2023
+ *      Author: olivas
+ */
+
+#ifndef AUX_FUNCTIONS_H_
+#define AUX_FUNCTIONS_H_
+
+#include <math.h>
+#include <stdio.h>
+
+void ftoa(float n, char* res, int afterpoint);
+
+
+#endif /* AUX_FUNCTIONS_H_ */
